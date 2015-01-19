a645720496944a068571964b513fd65e6522069b
Jason Norwood-Young <jason@10layer.com>
2015-01-19 09:40:40 +0200

    CORS

--- server.js
+++ server.js
@@ -140,6 +140,7 @@ var restify = require('restify');
 
 var server = restify.createServer();
 server.use(restify.queryParser());
+server.use(restify.CORS());
 
 var timePeriod = function(t) {
 	var periods = {
